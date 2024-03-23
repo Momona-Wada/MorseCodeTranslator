0eb557de043f37ecc5d95284fa59e9df541c005a
Momona Wada <momona.9410@gmail.com>
2024-03-22 17:22:34 -0700

    COMPLETE getUserInput.c

--- getUserInput.c
+++ getUserInput.c
@@ -1,1 +1,22 @@
+/*
+ * This program prompts the user to enter a message, and then converts the message to uppercase letters.
+ *
+ * Created by Momona Wada
+ * Date: 2024-03-21
+ */
+#include <stdio.h>
+#include <ctype.h>
 
+int main() {
+    char input[256];
+    printf("Enter your message: ");
+    fgets(input, sizeof(input), stdin);
+
+    // convert all letters to uppercase
+    for (int i = 0; input[i] != '\0'; i++) {
+        input[i] = toupper(input[i]);
+    }
+
+    printf("Message: %s", input);
+    return 0;
+}
