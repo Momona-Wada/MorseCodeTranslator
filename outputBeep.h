47a52848b78a2bcf3575a0c6d7ff11de9e9ffd0f
Tatsunori Marumo <tmarumo@my.bcit.ca>
2024-03-23 18:41:21 -0700

    create header for outputBeep

---
+++ outputBeep.h
@@ -0,0 +1,28 @@
+//
+// Created by Tatsunori Marumo on 3/23/2024.
+//
+
+#ifndef MORSECODETRANSLATOR_OUTPUTBEEP_H
+#define MORSECODETRANSLATOR_OUTPUTBEEP_H
+
+#if defined(_WIN64)
+#include <windows.h>
+#define PLAY_SOUND(file) Beep(FREQUENCY, file == "sound/dot.mov" ? DOT_TIME : DASH_TIME)
+#define SLEEP(ms) Sleep(ms)
+#else
+#include <unistd.h>
+#define PLAY_SOUND(file) system("afplay " file)
+#define SLEEP(ms) usleep((ms) * 1000)
+#endif
+
+#define DOT_TIME 200 // ms
+#define DASH_TIME 600 // ms
+#define BLANK_BETWEEN_SYMBOL 200 // ms
+#define BLANK_BETWEEN_CHARACTER 600 // ms
+#define FREQUENCY 700
+
+void dot();
+void dash();
+void outputBeep();
+
+#endif //MORSECODETRANSLATOR_OUTPUTBEEP_H
