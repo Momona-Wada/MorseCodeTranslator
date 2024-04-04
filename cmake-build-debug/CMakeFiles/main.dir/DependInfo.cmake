2e30e95bd47b389bfde870e5abcf98669c285f12
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-04-04 05:46:33 -0700

    Combine frontend with C program

---
+++ cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yoshidont_mind/Desktop/CST term2/COMP 2510 Procedural Programming/MorseCodeTranslator/convertToMorseCode.c" "CMakeFiles/main.dir/convertToMorseCode.c.o" "gcc" "CMakeFiles/main.dir/convertToMorseCode.c.o.d"
+  "/Users/yoshidont_mind/Desktop/CST term2/COMP 2510 Procedural Programming/MorseCodeTranslator/getUserInput.c" "CMakeFiles/main.dir/getUserInput.c.o" "gcc" "CMakeFiles/main.dir/getUserInput.c.o.d"
+  "/Users/yoshidont_mind/Desktop/CST term2/COMP 2510 Procedural Programming/MorseCodeTranslator/main.c" "CMakeFiles/main.dir/main.c.o" "gcc" "CMakeFiles/main.dir/main.c.o.d"
+  "/Users/yoshidont_mind/Desktop/CST term2/COMP 2510 Procedural Programming/MorseCodeTranslator/outputBeep.c" "CMakeFiles/main.dir/outputBeep.c.o" "gcc" "CMakeFiles/main.dir/outputBeep.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/convertToMorseCode.c.o"
+  "CMakeFiles/main.dir/convertToMorseCode.c.o.d"
+  "CMakeFiles/main.dir/getUserInput.c.o"
+  "CMakeFiles/main.dir/getUserInput.c.o.d"
+  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/main.c.o.d"
+  "CMakeFiles/main.dir/outputBeep.c.o"
+  "CMakeFiles/main.dir/outputBeep.c.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
